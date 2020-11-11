699a5388d2417cacaff7359c4914b8f7ad669ff6
Faysal Garaad <faysal.garaad01@gmail.com>
2020-11-10 19:49:11 -0500

    ..

--- Script.js
+++ Script.js
@@ -1,1 +1,1 @@
-//This website will be for streaming exclusively for streaming Islamic content, Since YouTube is suppressing most of them.
+//This website will be for streaming exclusively for streaming Islamic content, Since YouTube is suppressing most of them..
